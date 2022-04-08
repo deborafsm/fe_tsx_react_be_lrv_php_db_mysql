c11058ce2af15f3ce04cfa99eca4bb22a128b4df
VSTdeborafreire <62457927+deborafsm@users.noreply.github.com>
2022-04-08 12:19:15 -0300

    create: light and dark mode

---
+++ front_end_wallet/src/styles/themes/dark.ts
@@ -0,0 +1,17 @@
+export default {
+    title: 'dark',
+    color: {
+        primary: '#1b1d38',
+        secondary: '#252A48',
+        tertiary: '#313862',
+
+        white: '#fff',
+        black: '#000',
+        gray: '#BFBFBF',
+
+        success: '#4E41F0',
+        info: '#F7931B',
+        warning: "#E44C4E"
+
+    }
+}
