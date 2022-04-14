247a8800dce6f9bad19ef68edeb0c63b79f74302
VSTdeborafreire <62457927+deborafsm@users.noreply.github.com>
2022-04-14 17:31:10 -0300

    update aside

--- front_end_wallet/src/components/Aside/aside.tsx
+++ front_end_wallet/src/components/Aside/aside.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import {
     Container,
     Headers,
@@ -7,19 +8,38 @@ import {
     Title
 
 } from './style';
-import { FaLevelDownAlt,FaRegHeart, FaMoneyBill } from "react-icons/fa";
+import {
+    FaLongArrowAltDown,
+    FaLongArrowAltUp,
+    FaColumns,
+    FaMoneyBill,
+    FaDoorOpen
+} from "react-icons/fa";
 
 
 const Aside: React.FC = () => {
     return (
         <Container>
             <Headers>
-                <FaMoneyBill  size={50}/>
-                {/* <Title>Zhou Zi Shu <FaRegHeart/> Wen Ke Xing </Title> */}
+                <FaMoneyBill size={50} />
+                <Title>Wallet</Title>
             </Headers>
             <MenuContainer>
                 <MenuItemLink href="#">
+                    <FaColumns />
                     Dashboard
+                </MenuItemLink>
+                <MenuItemLink href="#">
+                    <FaLongArrowAltUp />
+                    Entradas
+                </MenuItemLink>
+                <MenuItemLink href="#">
+                    <FaLongArrowAltDown />
+                    Saidas
+                </MenuItemLink>
+                <MenuItemLink href="#">
+                    <FaDoorOpen />
+                    Sair
                 </MenuItemLink>
             </MenuContainer>
         </Container>
--- front_end_wallet/src/components/Aside/style.ts
+++ front_end_wallet/src/components/Aside/style.ts
@@ -1,25 +1,27 @@
 import styled from 'styled-components';
-
+// import {FaCoffee} from 'react-icons'
 export const Container = styled.div
     `
     grid-area: AS;
     color: ${props => props.theme.colors.white};
     background-color: ${props => props.theme.colors.primary};
     padding-left: 20px;
-    border-right: 1px solid ${props => props.theme.colors.gray}
+    border-right: 1px solid ${props => props.theme.colors.gray};
   
 `
     ;
 export const Headers = styled.header`
     display:flex;
     justify-content:center;
-    padding: 10px 0 10px 0
+    padding: 10px 0 10px 0;
+  
 
 `;
 
-export const MenuContainer = styled.div`
+export const MenuContainer = styled.nav`
+
     padding: 10px;    
-    border: 1px solid ${props => props.theme.colors.gray}
+    margin-top: 10px;
 `;
 
 
@@ -27,14 +29,23 @@ export const MenuItemLink = styled.a`
     display:flex;
     text-decoration: none;
     font-size: 1.3rem;
-    color: ${props => props.theme.colors.gray}
+    margin: 40px 0;
+    color: ${props => props.theme.colors.gray};
+    transition: opacity .3s;
+    &:hover{
+        opacity: .7;
+    }
+    >svg{
+        margin-right: 10px;
+    }
     
 `;
 export const Title = styled.h3`
-    display:flex;
-    
-    text-decoration: none;
-    font-size: 1.3rem;
-    color: ${props => props.theme.colors.gray}
-    
+    display: flex;
+    flex-direction: column-reverse;
+    justify-content: center;
+    margin: 10px;
+    font-size: 2em;
+    color: ${props => props.theme.colors.gray};
 `;
+
